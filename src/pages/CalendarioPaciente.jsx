1f2b4adef752598f1dd824b40cb769ccb899fffe
Arnol0 <72963047@certus.edu.pe>
2026-07-06 07:09:47 -0500

    Update CalendarioPaciente.jsx

--- src/pages/CalendarioPaciente.jsx
+++ src/pages/CalendarioPaciente.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import NavbarPaciente from "../components/common/NavbarPaciente";
 
 import StatsCalendario from "../components/calendario/Statscalendario";
-import CalendarioWidget from "../components/calendario/CalendarioWidget";
+import CalendarioWidget from "../components/calendario/Calendariowidget";
 import ProximaSesionCard from "../components/calendario/ProximaSesionCard";
 import TimelineActividades from "../components/calendario/TimelineActividades";
 
